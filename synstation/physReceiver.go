93d3e6ad59ed123bc507db5ed646ceda92d856f5
phler2 <lerouxp@yahoo.ca>
2011-02-08 17:07:27 +0100

    added shadow map creation and usage

--- synstation/physReceiver.go
+++ synstation/physReceiver.go
@@ -1,9 +1,15 @@
 package synstation
 
+// #include "math.h"
+//import "C"
+
 import "math"
 import "geom"
 import "container/list"
 import "fmt"
+import "rand"
+//import "sselib"
+
 
 // Structure to hold interference level, and multilevel interference with overlaping channels calculation
 // as well as the best signal and its recieved power
@@ -18,7 +24,7 @@ func (chR ChanReceiver) String() string { return fmt.Sprintf("{%f %f}", chR.Pint
 
 
 type PhysReceiverInt interface {
-	Init()
+	Init(r *rand.Rand)
 	EvalSignalConnection(ch int) (*ChanReceiver, float64)
 	EvalBestSignalSNR(ch int) (Rc *ChanReceiver, eval float64)
 	EvalSignalPr(e EmitterInt, ch int) (Pr, K float64)
@@ -42,14 +48,22 @@ type PhysReceiver struct {
 	geom.Pos
 	Channels    []ChanReceiver
 	Orientation []float64 //angle of orientation for beamforming for each channel -1 indicates no beamforming
+	shadow      shadowMapInt
 }
 
-func (r *PhysReceiver) Init() {
+func (r *PhysReceiver) Init(Rgen *rand.Rand) {
 	r.Channels = make([]ChanReceiver, NCh)
 	r.Orientation = make([]float64, NCh)
 	for i := 0; i < len(r.Orientation); i++ {
 		r.Orientation[i] = -1
 	}
+	switch SetShadowMap {
+	case NOSHADOW:
+		r.shadow = new(noshadow)
+	case SHADOWMAP:
+		r.shadow = new(shadowMap)
+	}
+	r.shadow.Init(corr_res, Rgen)
 }
 
 func (r *PhysReceiver) SetPos(p geom.Pos) {
@@ -230,17 +244,14 @@ func (rx *PhysReceiver) RicePropagation(E EmitterInt) (fading float64, K float64
 	a := d + 2
 	a = a * a
 	a = a * a
-	fading = (1.0 + rx.SlowFading(E.GetPos())) / a
+	fading = (1.0 * rx.SlowFading(E.GetPos())) / a
 	K = 1 / (d + 1)
 	//K=0;
 	return
 }
 
 func (rx *PhysReceiver) SlowFading(E *geom.Pos) (c float64) {
-	//	a:=math.Sin(E.X/math.Pi/300.0) 
-	//	b:=math.Cos(E.Y/math.Pi/320.0)
-	//	return a*a * b*b  +.02;
-	return 0.0
+	return rx.shadow.evalShadowFading(E.Minus(rx.Pos))
 }
 
 func (rx *PhysReceiver) DoTracking(Connec *list.List) bool {
@@ -264,5 +275,130 @@ func (rx *PhysReceiver) DoTracking(Connec *list.List) bool {
 	}
 
 	return false
+}
+
+
+type shadowMapInt interface {
+	Init(corr float64, Rgen *rand.Rand)
+	evalShadowFading(d geom.Pos) (val float64)
+}
+
+
+type noshadow struct{}
+
+func (s *noshadow) Init(f float64, rgen *rand.Rand) {
+}
+func (s *noshadow) evalShadowFading(d geom.Pos) (val float64) {
+	return 1.0
+}
+
+type shadowMap struct {
+	xcos []float64
+	ycos []float64
+
+	xsin []float64
+	ysin []float64
+
+	power float64
+
+	smap [][]float32
+}
+/*
+type float32x4 struct {
+	A, B, C, D float32
+}
+
+func (f float32x4) mult(f float32x4) float32 {
+
+}*/
+
+
+var mapres = mapsize / float64(maplength)
+
+func (s *shadowMap) Init(corr_dist float64, Rgen2 *rand.Rand) {
+
+	nval := int(Field / corr_dist / shadow_sampling)
+
+	//fmt.Println(" shadowMap down Sampling ", shadow_sampling, " ", nval)
+
+	s.xcos = make([]float64, nval)
+	s.ycos = make([]float64, nval)
+	s.xsin = make([]float64, nval)
+	s.ysin = make([]float64, nval)
+
+	for i := 0; i < nval; i++ {
+		s.xcos[i] = Rgen2.NormFloat64()
+		//s.xcos[i] *= s.xcos[i]
+		s.ycos[i] = Rgen2.NormFloat64()
+
+		s.xsin[i] = Rgen2.Float64() * 2 * math.Pi
+		s.ysin[i] = Rgen2.Float64() * 2 * math.Pi
+
+		if s.xcos[i] < mval {
+			s.xcos[i] = 0
+		}
+		if s.ycos[i] < mval {
+			s.ycos[i] = 0
+		}
+		s.power += s.xcos[i] * s.xcos[i]
+		s.power += s.ycos[i] * s.ycos[i]
+
+	}
+
+	s.power = math.Sqrt(s.power) / shadow_deviance
+
+	for i := 0; i < nval; i++ {
+		s.xcos[i] /= s.power
+		s.ycos[i] /= s.power
+	}
+
+	s.smap = make([][]float32, mapsize)
+	for i := 0; i < mapsize; i++ {
+		s.smap[i] = make([]float32, mapsize)
+		x := (float64(i) - mapsize/2) / mapres
+		for j := 0; j < mapsize; j++ {
+			d := geom.Pos{x, (float64(j) - mapsize/2) / mapres}
+			s.smap[i][j] = float32(s.evalShadowFadingDirect(d))
+			//lets not have -Inf here			
+			if s.smap[i][j] < 0.0000001 {
+				s.smap[i][j] = 0.0000001
+			}
+		}
+	}
+
+}
+
+func (s *shadowMap) interpolFading(d geom.Pos) (val float64) {
+
+	x := int(d.X*mapres + mapsize/2)
+	y := int(d.Y*mapres + mapsize/2)
+
+	if x < 0 || x >= mapsize || y < 0 || y >= mapsize {
+		return 1.0
+	}
+
+	return float64(s.smap[x][y])
+
+}
+
+var facr = float64(2.0 * math.Pi / Field)
+
+func (s *shadowMap) evalShadowFading(d geom.Pos) (val float64) {
+	return s.interpolFading(d)
+	//return s.evalShadowFadingDirect(d)
+}
+
+func (s *shadowMap) evalShadowFadingDirect(d geom.Pos) float64 {
+
+	posx := float64(d.X) * facr * shadow_sampling
+	posy := float64(d.Y) * facr * shadow_sampling
+	var rx, ry, val float64
+	for i := 0; i < len(s.xcos); i++ {
+		rx += posx
+		ry += posy
+		val += s.xcos[i]*(math.Cos((rx + s.xsin[i]))) + s.ycos[i]*(math.Cos((ry + s.ysin[i])))
+	}
+
+	return math.Pow(10, val/10)
 }
 
