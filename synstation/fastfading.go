6b0c160720e2b4ba784e0513b84f0c55d728c3d0
phler2 <lerouxp@yahoo.ca>
2011-03-25 10:05:49 +0100

    delete, no more used incorporated into physReceiver.go

--- synstation/fastfading.go
+++ synstation/fastfading.go
@@ -9,20 +9,14 @@ type FadingData struct {
 	ff_R [M]float64
 }
 
-const K = 0
 
-func (f *FadingData) GenerateFading(Rgen *rand.Rand) {
-
+func (f *FadingData) GenerateFading(Rgen *rand.Rand, p PowerData) {
+	K := float64(0)
 	for i := 0; i < M; i++ {
 		a := Rgen.NormFloat64()
 		b := Rgen.NormFloat64()
 		f.ff_R[i] = ((K+a)*(K+a) + b*b) / 2
-
-		//fmt.Println(f.ff_R[i])
+		f.ff_R[i] *= p.pr[i]
 	}
-}
-
-func (f *FadingData) GetFastFading(m int) float64 {
-	return f.ff_R[m]
 }
 
--- synstation/powerData.go
+++ synstation/powerData.go
@@ -6,19 +6,14 @@ type PowerData struct {
 }
 
 
-func (p *PowerData) CalculatePr(rx *PhysReceiver, FF FadingData ) {
+func (p *PowerData) CalculatePr(rx *PhysReceiver, FF FadingData) {
 
 	for i := 0; i < M; i++ {
 
-		E:=&Mobiles[i]
+		E := &Mobiles[i]
 		gain := rx.GainBeam(E, E.GetCh())
 		fading, _ := rx.Fading(E)
 		p.pr[i] = fading * gain * E.GetPower()
-
 	}
-}
-
-func (p *PowerData) GetPr(m int) float64 {
-	return p.pr[m]
 }
 
