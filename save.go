24888da0416e1cef0d4746cbec39457af44d5dd6
phler2 <lerouxp@yahoo.ca>
2011-03-25 09:28:17 +0100

    optimize writing string using buffer +code formating

--- save.go
+++ save.go
@@ -4,6 +4,7 @@ import "synstation"
 import "os"
 import "fmt"
 import "container/vector"
+import "bytes"
 
 var syncsavech chan int
 
@@ -11,79 +12,86 @@ var saveData vector.Vector
 
 func init() {
 
-	syncsavech= make(chan int)
-	
-//	saveData.Push(CreateStart(MaxBER, synstation.M, "BERMax"))
-//	saveData.Push(CreateStart(InstMaxBER, synstation.M, "InstMatBER"))
-//	saveData.Push(CreateStart(BER, synstation.M, "BER"))
-//	saveData.Push(CreateStart(SNR, synstation.M, "SNR"))
-//	saveData.Push(CreateStart(CH, synstation.M, "CH"))
-//	saveData.Push(CreateStart(DIV, synstation.M, "DIV"))
-//	saveData.Push(CreateStart(Outage, synstation.M, "Outage"))
-//	saveData.Push(CreateStart(Ptxr, synstation.M, "Ptxr"))
+	syncsavech = make(chan int)
+
+	//	saveData.Push(CreateStart(MaxBER, synstation.M, "BERMax"))
+	//	saveData.Push(CreateStart(InstMaxBER, synstation.M, "InstMatBER"))
+	saveData.Push(CreateStart(BER, synstation.M, "BER"))
+	//	saveData.Push(CreateStart(SNR, synstation.M, "SNR"))
+	//saveData.Push(CreateStart(CH, synstation.M, "CH"))
+	//saveData.Push(CreateStart(DIV, synstation.M, "DIV"))
+	saveData.Push(CreateStart(Outage, synstation.M, "Outage"))
+	//	saveData.Push(CreateStart(Ptxr, synstation.M, "Ptxr"))
 }
 
-type saveTraceItem struct{
+type saveTraceItem struct {
 	save chan *synstation.Trace
 }
 
-func CreateStart(method func (t *synstation.Trace, i int) float64 , m int , file string ) saveTraceItem{
+func CreateStart(method func(t *synstation.Trace, i int) float64, m int, file string) saveTraceItem {
 	var a saveTraceItem
 	a.save = make(chan *synstation.Trace, 1000)
 	go WriteDataToFile(method, m, a.save, file)
 	return a
 }
 
-func (s *saveTraceItem) Stop(){
+func (s *saveTraceItem) Stop() {
 	close(s.save)
-	<- syncsavech
+	<-syncsavech
 }
 
-func MaxBER( t *synstation.Trace,  i int) float64 { return t.Mobs[i].MaxBER}  
-func InstMaxBER( t *synstation.Trace,  i int) float64 { return t.Mobs[i].InstMaxBER} 
-func BER( t *synstation.Trace,  i int) float64 { return t.Mobs[i].BERtotal} 
-func SNR( t *synstation.Trace,  i int) float64 { return t.Mobs[i].SNRb} 
-func CH( t *synstation.Trace,  i int) float64 { return float64(t.Mobs[i].Ch)} 
-func DIV( t *synstation.Trace,  i int) float64 { return float64(t.Mobs[i].Diversity)} 
-func Outage( t *synstation.Trace,  i int) float64 { return float64(t.Mobs[i].Outage)} 
-func Ptxr( t *synstation.Trace,  i int) float64 { return float64(t.Mobs[i].Power)} 
+func MaxBER(t *synstation.Trace, i int) float64     { return t.Mobs[i].MaxBER }
+func InstMaxBER(t *synstation.Trace, i int) float64 { return t.Mobs[i].InstMaxBER }
+func BER(t *synstation.Trace, i int) float64        { return t.Mobs[i].BERtotal }
+func SNR(t *synstation.Trace, i int) float64        { return t.Mobs[i].SNRb }
+func CH(t *synstation.Trace, i int) float64         { return float64(t.Mobs[i].Ch) }
+func DIV(t *synstation.Trace, i int) float64        { return float64(t.Mobs[i].Diversity) }
+func Outage(t *synstation.Trace, i int) float64     { return float64(t.Mobs[i].Outage) }
+func Ptxr(t *synstation.Trace, i int) float64       { return float64(t.Mobs[i].Power) }
 
-func WriteDataToFile( method func (t *synstation.Trace, i int) float64 , m int, channel chan *synstation.Trace, file string )  { 
+func WriteDataToFile(method func(t *synstation.Trace, i int) float64, m int, channel chan *synstation.Trace, file string) {
 
 	outF, err := os.Open(file+".mat", os.O_WRONLY, 0666)
 	fmt.Println(err)
-	outF.WriteString(fmt.Sprintln("# name: ",file,"\n# type: matrix\n# rows: ", synstation.Duration, "\n# columns: ", m))
+	outF.WriteString(fmt.Sprintln("# name: ", file, "\n# type: matrix\n# rows: ", synstation.Duration, "\n# columns: ", m))
 
 	for t := range channel {
-		var s string
-		for i := 0; i < m; i++ {	
-		s += fmt.Sprintf("%1.2f ",method(t,i))
+
+		/*var s string
+		for i := 0; i < m; i++ {
+			s += fmt.Sprintf("%1.2f ", method(t, i))
 		}
 
-		outF.WriteString(s +"\n")
+		outF.WriteString(s + "\n")
+		*/
+
+		buffer := bytes.NewBufferString("")
+
+		for i := 0; i < m; i++ {
+			fmt.Fprint(buffer, method(t, i))
+			fmt.Fprint(buffer, " ")
+		}
+		outF.WriteString(string(buffer.Bytes()))
 
 	}
 
-	
-	
-
 	outF.Close()
 
-	syncsavech <-1
+	syncsavech <- 1
 
-} 
+}
 
-func StopSave(){
-	for i:= 0 ; i< len(saveData); i++ {
-		a:=saveData.At(i).(saveTraceItem)
+func StopSave() {
+	for i := 0; i < len(saveData); i++ {
+		a := saveData.At(i).(saveTraceItem)
 		a.Stop()
 	}
 }
 
-func sendTrace(t *synstation.Trace){
+func sendTrace(t *synstation.Trace) {
 
-	for i:= 0 ; i< len(saveData); i++ {
-		a:=saveData.At(i).(saveTraceItem)
+	for i := 0; i < len(saveData); i++ {
+		a := saveData.At(i).(saveTraceItem)
 		a.save <- t
 	}
 }
