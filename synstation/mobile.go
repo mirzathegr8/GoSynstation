20eb7a700ec4f8f8a231d5d4b306c9cafede9768
phler2 <lerouxp@yahoo.ca>
2011-04-01 14:15:06 +0200

    changed Speed initialisation to take into account  TTI of 1ms, and better uniform angles...

--- synstation/mobile.go
+++ synstation/mobile.go
@@ -2,6 +2,7 @@ package synstation
 
 import "rand"
 //import "fmt"
+import "math"
 
 
 //  a mobile is an emitter with mobility : speed data, 
@@ -20,7 +21,7 @@ func (M *Mob) Init(i int) {
 	M.Id = i
 	M.Rgen = rand.New(rand.NewSource(Rgen.Int63()))
 
-	M.Requested = -3.0
+	M.Requested = -4.0
 
 	M.X = M.Rgen.Float64() * Field
 	M.Y = M.Rgen.Float64() * Field
@@ -30,8 +31,11 @@ func (M *Mob) Init(i int) {
 
 	SystemChan[0].Change <- &M.Emitter
 
-	M.Speed[0] = (M.Rgen.Float64()*2 - 1) * MaxSpeed
-	M.Speed[1] = (M.Rgen.Float64()*2 - 1) * MaxSpeed
+	speed := M.Rgen.Float64() * MaxSpeed / 1000
+	//speed := float64(0.00)
+	angle := M.Rgen.Float64() * 2 * math.Pi
+	M.Speed[0] = speed * math.Cos(angle)
+	M.Speed[1] = speed * math.Sin(angle)
 
 }
 
@@ -81,11 +85,11 @@ func (M *Mob) RunPhys() {
 // finnaly sents to syncchannel BER level
 func (M *Mob) FetchData() {
 
-	if (M.SBERtotal==0){ M.Outage++
-	}else{
-		M.Outage=0
+	if M.SBERtotal == 0 {
+		M.Outage++
+	} else {
+		M.Outage = 0
 	}
-
 
 	M.BERtotal, M.Diversity, M.MaxBER, M.InstMaxBER = M.SBERtotal, M.SDiversity, M.SMaxBER, M.SInstMaxBER
 	M.SInstMaxBER, M.SBERtotal, M.SDiversity, M.SMaxBER = 0, 0, 0, 0
