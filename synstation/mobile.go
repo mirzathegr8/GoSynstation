2d9db49f7491f153b917a20842c4c8b29c53baa4
phler2 <lerouxp@yahoo.ca>
2011-04-18 18:37:27 +0200

    moved FetchData to emitter from mobile as it is related to the inerfunction of the emitter and not the mobile-agent
    refactored runagent/move

--- synstation/mobile.go
+++ synstation/mobile.go
@@ -1,7 +1,6 @@
 package synstation
 
 import "rand"
-//import "fmt"
 import "math"
 
 
@@ -12,8 +11,6 @@ type Mob struct {
 	//R    PhysReceiver
 	Rgen  *rand.Rand
 	clock int
-
-	//meanBERInstTot MeanData
 }
 
 
@@ -22,7 +19,7 @@ func (M *Mob) Init(i int) {
 	M.Id = i
 	M.Rgen = rand.New(rand.NewSource(Rgen.Int63()))
 
-	M.Requested = -15.0
+	M.Requested = -7.0
 
 	M.X = M.Rgen.Float64() * Field
 	M.Y = M.Rgen.Float64() * Field
@@ -38,13 +35,10 @@ func (M *Mob) Init(i int) {
 
 }
 
-func (M *Mob) getInt() EmitterInt {
-	return M
-}
+// 	applies agent functionality (move mobiles)
+func (M *Mob) RunAgent() {
 
-
-func (M *Mob) move() {
-
+	//Move the mobile
 	M.X += M.Speed[0]
 	M.Y += M.Speed[1]
 
@@ -64,84 +58,18 @@ func (M *Mob) move() {
 		M.Y = Field
 	}
 
+	SyncChannel <- 1.0
 }
 
 
 func (M *Mob) RunPhys() {
 
-	M.clock = M.Rgen.Intn(15)
-	if M.clock == 1 {
-		//	M.R.MeasurePower(M)
-	}
+	//M.clock = M.Rgen.Intn(15)
+	//if M.clock == 1 {
+	//	M.R.MeasurePower(M)
+	//}
 
 	SyncChannel <- 1
 
-}
-
-// this function saves totalBER and inform which dbs connection is master
-// reset power and channel if all connections losts
-// applies agent functionality (move mobiles)
-// finnaly sents to syncchannel BER level
-func (M *Mob) FetchData() {
-
-	M.BERtotal, M.Diversity, M.MaxBER, M.InstMaxBER = M.SBERtotal, M.SDiversity, M.SMaxBER, M.SInstMaxBER
-	M.SInstMaxBER, M.SBERtotal, M.SDiversity, M.SMaxBER = 0, 0, 0, 0
-
-	M.TransferRate = 0
-
-	M.Outage++
-	for rb := 1; rb < NCh; rb++ {
-		if M.IsSetARB(rb) {
-			/*M.SBERrb[rb] = 0
-			pe := L1 * math.Exp(-M.SSNRrb[rb]/2/L2) / 2.0
-			for i := 0; i < 10; i++ {
-				M.SBERrb[rb] += math.Pow(1-pe, 1024-float64(i)) *
-					math.Pow(pe, float64(i)) * factorial[i]
-			}
-			M.SBERrb[rb] = 1 - M.SBERrb[rb]*/
-
-			//M.meanBERInstTot.Add(M.SSNRrb[rb]) //for now as we use only 1 rb
-
-			/*if M.Diversity > 0 {
-				M.TransferRate = L1/2.0*math.Exp(-M.SSNRrb[rb]/2/L2) + 1e-40
-			} else {
-				M.TransferRate = 1
-
-			}*/
-
-			M.TransferRate += 80 * math.Log2(1+M.SSNRrb[rb])
-
-			if 100 < M.TransferRate {
-
-				M.Outage = 0
-
-			} else {
-				//M.Rate--
-				M.TransferRate = 0
-
-			}
-
-			//M.TransferRate = math.Log10(M.TransferRate)
-
-
-		}
-		M.SSNRrb[rb] = 0
-	}
-
-	if M.BERtotal == 0 && !M.IsSetARB(0) {
-		M.MasterConnection = nil
-		M.Power = 1
-		M.ReSetARB()
-	} else if M.MasterConnection != nil {
-		M.MasterConnection.Status = 0 // we are master
-	}
-
-	if M.IsSetARB(0) {
-		SyncChannel <- 0.0
-	} else {
-		SyncChannel <- M.BERtotal
-	}
-
-	M.move()
 }
 
