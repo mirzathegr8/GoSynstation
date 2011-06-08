e5d4b7ab65a58cb3e43805df6e586749c88bf128
phler2 <lerouxp@yahoo.ca>
2011-06-08 21:02:30 +0200

    added labels and metrix for fairness, which needs to be evaluated

--- PlotResults.m
+++ PlotResults.m
@@ -3,12 +3,16 @@
 figure(1);
 load TransferRate.mat ;
 plot(sort(sum(TransferRate,2)),c);
+ylabel ("capacity")
+xlabel("mobiles")
 hold on;
 
 figure(2);
 load Outage.mat;
 [rr,T]=outage(Outage);
 plot(log10(rr+1),c);
+ylabel ("number of mobiles in outage")
+xlabel("TTI")
 hold on;
 
 figure(3);
@@ -28,4 +32,12 @@ plot(mean(TransferRate,2)(I),c);
 hold on
 
 
+'Total TransferRate'
 sum(sum(TransferRate))
+'Fairness square error'
+tt=sum(TransferRate,2);
+tm=(tt-mean(tt));
+sum(tm(tm<0).^2)
+'Fairness square error2'
+tm=(tt-median(tt));
+sum(tm(tm<0).^2)
