ac25e2395aff91ba32836d651d78701ff74597d5
phler2 <lerouxp@yahoo.ca>
2011-08-22 09:17:26 +0200

    minor changes

--- PlotResults.m
+++ PlotResults.m
@@ -1,13 +1,13 @@
 
 
-figure(1);
+figure(1+fign);
 load TransferRate.mat ;
 plot(sort(sum(TransferRate,2)/size(TransferRate,2)*1000/1e6),c);
 %ylabel ("capacity")
 xlabel("mobiles")
 hold on;
 
-figure(2);
+figure(2+fign);
 load Outage.mat;
 [rr,T]=outage(Outage);
 plot(log10(rr/size(TransferRate,1)*1000+1),c);
@@ -15,10 +15,10 @@ plot(log10(rr/size(TransferRate,1)*1000+1),c);
 xlabel("TTI")
 hold on;
 
-figure(3);
-load Ptxr.mat;
-plot(sort(mean(Ptxr,2)),c);
-hold on;
+#figure(3);
+#load Ptxr.mat;
+#plot(sort(mean(Ptxr,2)),c);
+#hold on;
 
 
 figure(4);
@@ -27,17 +27,17 @@ load NumARB.mat;
 plot(V,c);
 hold on;
 
-figure(5);
-plot(mean(TransferRate,2)(I),c);
-hold on
+#figure(5);
+#plot(mean(TransferRate,2)(I),c);
+#hold on
 
 
-'Total TransferRate'
-sum(sum(TransferRate))
-'Fairness square error'
-tt=sum(TransferRate,2);
-tm=(tt-mean(tt));
-sum(tm(tm<0).^2)
-'Fairness square error2'
-tm=(tt-median(tt));
-sum(tm(tm<0).^2)
+#'Total TransferRate'
+#sum(sum(TransferRate))
+#'Fairness square error'
+#tt=sum(TransferRate,2);
+#tm=(tt-mean(tt));
+#sum(tm(tm<0).^2)
+#'Fairness square error2'
+#tm=(tt-median(tt));
+#sum(tm(tm<0).^2)
