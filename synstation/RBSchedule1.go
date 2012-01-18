c70c64e6d05a07dadefddf2c605d272136decbea
phler2 <lerouxp@yahoo.ca>
2012-01-18 11:00:19 +0100

    cleaned for direct acces to var. restructured for memory data struct and Scheduler struct

--- synstation/RBSchedule1.go
+++ synstation/RBSchedule1.go
@@ -7,11 +7,24 @@ import "math"
 //import "fmt"
 //import "geom"
 
+type ARBScheduler1 struct {
+	Metric [NConnec][NCh]float64
+	MasterMobs [NConnec]*Emitter
 
-func ARBScheduler(dbs *DBS, Rgen *rand.Rand) {
+	AL [NCh]int
+	NumAss [NConnec]int
+	CAL [NCh]int
+}
 
-	var Metric [NConnec][NCh]float64
-	var MasterMobs [NConnec]*Emitter
+
+func initARBScheduler1() Scheduler {
+	d := new(ARBScheduler1)	
+	return d
+}
+
+func (d *ARBScheduler1) Schedule(dbs *DBS, Rgen *rand.Rand) {
+
+	
 	Nmaster :=0
 
 	r:= dbs.Pos
@@ -22,24 +35,24 @@ func ARBScheduler(dbs *DBS, Rgen *rand.Rand) {
 		c := e.Value.(*Connection)
 		if c.Status==0 {
 		
-			E := c.GetE()
-			m_m := E.GetMeanTR()	
+			E := c.E
+			m_m := E.meanTR.Get()
 
 			for rb := 0; rb < NCh; rb++ {	
 
-				snrrb := E.GetSNRrb(rb)
+				snrrb := E.SNRrb[rb]
 	
 				m := EffectiveBW * math.Log2(1+beta*snrrb)
 
 				if m > 100 {
-					Metric[Nmaster][rb] = math.Log2(m + 1)
+					d.Metric[Nmaster][rb] = math.Log2(m + 1)
 					b := (m_m + 1)			
-					Metric[Nmaster][rb] /= b				
+					d.Metric[Nmaster][rb] /= b				
 				}					
 			}
-			MasterMobs[Nmaster]=E	
+			d.MasterMobs[Nmaster]=E	
 
-			ICIMfunc(&r,E,Metric[Nmaster][:],dbs.Color)
+			ICIMfunc(&r,E,d.Metric[Nmaster][:],dbs.Color)
 		
 			Nmaster++
 
@@ -53,48 +66,50 @@ func ARBScheduler(dbs *DBS, Rgen *rand.Rand) {
 
 	
 
-	var CAL [NCh]int
+	
 	MaxMP:=0.0
 	for g :=0 ;g<1;g++{
 
-		MetricBis:=Metric
+		MetricBis:=d.Metric
 
-		var AL [NCh]int
-		var NumAss [NConnec]int
+		
+		
 		//First assign RB to best Metric	
 		MetricPool:=0.0
 		RBrange := Rgen.Perm(NCh-NChRes) //add NChRes
-		AL[0]=-1
+
+		for j:=range d.AL { d.AL[j]=-1}
+		for j:=range d.NumAss {d.NumAss[j]=0}
 
 		//for rb := 1; rb < NCh; rb++ {
 		for _,rbo:= range RBrange{
 			rb:=rbo+NChRes
-			AL[rb] = -1
-			for i :=range MasterMobs[0:Nmaster] {			
+			d.AL[rb] = -1
+			for i :=range d.MasterMobs[0:Nmaster] {			
 				if MetricBis[i][rb] > 0 {
-					if AL[rb] < 0 {
-						AL[rb] = i
-					} else if MetricBis[i][rb] > MetricBis[AL[rb]][rb] {
-						AL[rb] = i
+					if d.AL[rb] < 0 {
+						d.AL[rb] = i
+					} else if MetricBis[i][rb] > MetricBis[d.AL[rb]][rb] {
+						d.AL[rb] = i
 					}
 				}
 			}
-			if AL[rb] >= 0 {
-				MetricPool+=MetricBis[AL[rb]][rb]
-				NumAss[AL[rb]]++ //this emitter will have one more assigned RB
+			if d.AL[rb] >= 0 {
+				MetricPool+=MetricBis[d.AL[rb]][rb]
+				d.NumAss[d.AL[rb]]++ //this emitter will have one more assigned RB
 				for rb2 := 1; rb2 < NCh; rb2++ {
-					MetricBis[AL[rb]][rb2] *= float64(NumAss[AL[rb]]) / float64(NumAss[AL[rb]]+1)
+					MetricBis[d.AL[rb]][rb2] *= float64(d.NumAss[d.AL[rb]]) / float64(d.NumAss[d.AL[rb]]+1)
 				}
 			}
 		}
 		if MetricPool>MaxMP {	
 			MaxMP=MetricPool
-			CAL=AL
+			d.CAL=d.AL
 		}
 	}
 	
 
-	Allocate(CAL[:],MasterMobs[0:Nmaster])
+	Allocate(d.CAL[:],d.MasterMobs[0:Nmaster])
 	//AllocateOld(AL[:], dbs )
 	
 	
