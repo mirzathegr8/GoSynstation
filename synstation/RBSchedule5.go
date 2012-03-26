4010857712e7a708c2da335b70ebc5dbecc20ac1
Philippe Leroux <lerouxp@alapuce.(none)>
2012-03-26 10:16:29 -0400

    cleaned code for new memory data structure and shceduler interface

--- synstation/RBSchedule5.go
+++ synstation/RBSchedule5.go
@@ -14,15 +14,38 @@ func init() {
 	fmt.Println("init to keep fmt")
 }
 
-func ARBScheduler5(dbs *DBS, Rgen *rand.Rand) {
 
-	var Metric [NConnec][NCh]float64
-	var metricpool [popsize * 11]float64
-	var index [popsize * 11]int
-	var S Sequence
-	S.index = index[0 : popsize*11]
+// memory for the scheduler
+type ARBScheduler5 struct {
+	Metric [NConnec][NCh]float64
+	metricpool [popsize * 11]float64
+	index [popsize * 11]int
+	S Sequence
+	MasterMobs [NConnec]*Emitter
 
-	var MasterMobs [NConnec]*Emitter
+	PopulAr [popsize][NCh]int
+	Popul [popsize]allocSet
+	
+	poolAr [(popsize + 1) * generations][NCh]int
+	pool [(popsize + 1) * 10]allocSet
+	AL [NCh]int
+}
+
+func initARBScheduler5() Scheduler {
+	d := new(ARBScheduler5)
+	d.S.index = d.index[0 : popsize*11]
+	for i := range d.Popul {
+		d.Popul[i].vect = d.PopulAr[i][:]
+	}
+	for i := range d.pool {
+		d.pool[i].vect = d.poolAr[i][:]
+	}
+
+	return d
+}
+
+func (d *ARBScheduler5) Schedule(dbs *DBS, Rgen *rand.Rand) {
+
 
 	// Eval Metric for all connections
 	var Nmaster int
@@ -38,7 +61,7 @@ func ARBScheduler5(dbs *DBS, Rgen *rand.Rand) {
 
 		if c.Status == 0 {
 
-			MasterMobs[Nmaster] = E
+			d.MasterMobs[Nmaster] = E
 			for rb := 1; rb < NCh; rb++ {
 				var snrrb float64
 				if DiversityType == SELECTION {
@@ -46,10 +69,12 @@ func ARBScheduler5(dbs *DBS, Rgen *rand.Rand) {
 				} else {
 					snrrb = E.SNRrb[rb]
 				}
-				Metric[Nmaster][rb] = snrrb
+			if E.ARB[rb] {snrrb*=float64(E.GetNumARB())} //since we redivide the power according to the new number of allocated RBs
+		
+				d.Metric[Nmaster][rb] = snrrb
 			}
 
-			ICIMfunc(&r, E, Metric[Nmaster][:], dbs.Color)
+			ICIMfunc(&r, E, d.Metric[Nmaster][:], dbs.Color)
 
 			Nmaster++
 		}
@@ -59,31 +84,14 @@ func ARBScheduler5(dbs *DBS, Rgen *rand.Rand) {
 		return
 	}
 
-	//Assign RB for master connections
-
-	//var NumAss [NConnec]int
-
-	//	var Popul [popsize][NCh]int	
-	//	var pool [popsize * 11][NCh]int
-
-	var PopulAr [popsize][NCh]int
-	var Popul [popsize]allocSet
-	for i := range Popul {
-		Popul[i].vect = PopulAr[i][:]
-	}
-
-	var poolAr [(popsize + 1) * generations][NCh]int
-	var pool [(popsize + 1) * 10]allocSet
-	for i := range pool {
-		pool[i].vect = poolAr[i][:]
-	}
+	
 
 	//First assign RB to best Metric
 	for i := 0; i < popsize; i++ {
 		nbrb := int(float64(NCh) / (float64(Nmaster))) //int(float64(NCh) * float64(r) / float64(numberAmobs))
 		a := Rgen.Perm(Nmaster)
 		//expand
-		pp := Popul[i].vect
+		pp := d.Popul[i].vect
 		// first dealocate everything
 		for j := 0; j < NCh; j++ {
 			pp[j] = -1
@@ -100,33 +108,33 @@ func ARBScheduler5(dbs *DBS, Rgen *rand.Rand) {
 	for gen := 0; gen < generations; gen++ {
 
 		for j := 0; j < popsize; j++ {
-			createDesc(Popul[j], pool[j*10:(j+1)*10], Rgen)
+			createDesc(d.Popul[j], d.pool[j*10:(j+1)*10], Rgen)
 		}
 
-		for i := range Popul {
-			copy(pool[popsize*10+i].vect, Popul[i].vect)
+		for i := range d.Popul {
+			copy(d.pool[popsize*10+i].vect, d.Popul[i].vect)
 		}
 		//copy(pool[popsize*10:popsize*11].vect, Popul[:].vect)
 
 		//select the new population
 
 		for i := 0; i < popsize*11; i++ {
-			var AL [NCh]int
-			copy(AL[:], pool[i].vect) //copies 
-			AL[0] = -1
-			metricpool[i] = Trimm(AL[:], &Metric, MasterMobs[0:Nmaster])
+			
+			copy(d.AL[:], d.pool[i].vect) //copies 
+			d.AL[0] = -1
+			d.metricpool[i] = Trimm(d.AL[:], &d.Metric, d.MasterMobs[0:Nmaster])
 		}
 
 		//find the best 100
-		initSequence(metricpool[:], &S)
-		sort.Sort(S)
-		for i := 0; i < len(Popul); i++ {
-			Popul[i] = pool[S.index[i]]
+		initSequence(d.metricpool[:], &d.S)
+		sort.Sort(d.S)
+		for i := 0; i < len(d.Popul); i++ {
+			d.Popul[i] = d.pool[d.S.index[i]]
 		}
 
 	}
 
-	AL := Popul[0].vect
+	ALf := d.Popul[0].vect
 
 	//Trimm we delete endings of allocation sequence if the capacity of these RB is not that good
 	//this is to prevent spendin too much energy for little gain, and also to give a chance to minimize interference
@@ -137,7 +145,7 @@ func ARBScheduler5(dbs *DBS, Rgen *rand.Rand) {
 
 	//copy(dbs.ALsave[:],AL)
 
-	Allocate(AL[:], MasterMobs[0:Nmaster])
+	Allocate(ALf[:], d.MasterMobs[0:Nmaster])
 	//AllocateOld(AL[:],dbs)
 
 }
