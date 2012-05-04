cb304e0552fccfddcd35ae727ed4f80e7f1a0f0a
Philippe Leroux <lerouxp@alapuce.(none)>
2012-05-04 18:02:12 -0400

    output print enhance

--- main/runtimeOutput.go
+++ main/runtimeOutput.go
@@ -58,18 +58,34 @@ func (o *outputData) Div(k float64) {
 }
 
 func (o outputData) String() string {
-	return fmt.Sprint(o.listened, "	", o.connected, "	",
-		o.BER1, "	",		
-		o.BER3, "	",
-		o.d_connec, "	",
-		o.d_discon, "	",
-		o.lost, "	",
-		o.d_lost, "	",
-		o.Diversity, "	",
-		o.HopCount, "	",
-		o.Fairness, "	",
-		o.SumTR, "	",
+//	return fmt.Sprint(o.listened, "	", o.connected, "	",
+//		o.BER1, "	",		
+//		o.BER3, "	",
+//		o.d_connec, "	",
+//		o.d_discon, "	",
+//		o.lost, "	",
+//		o.d_lost, "	",
+//		o.Diversity, "	",
+//		o.HopCount, "	",
+//		o.Fairness, "	",
+//		o.SumTR, "	",
+//		o.MUFactor)
+
+return fmt.Sprintf("%d	%d	%d	%d	%d	%d	%d	%d	%1.2f	%d	%1.2f	%1.2e	%4.2f",
+		int(o.listened), 
+		int(o.connected),
+		int(o.BER1), 		
+		int(o.BER3), 
+		int(o.d_connec), 
+		int(o.d_discon), 
+		int(o.lost), 
+		int(o.d_lost), 
+		o.Diversity, 
+		int(o.HopCount), 
+		o.Fairness, 
+		o.SumTR, 
 		o.MUFactor)
+
 
 }
 
